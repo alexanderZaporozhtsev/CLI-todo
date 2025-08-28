14e6f356db6589b52781e5a72f6956dc9e915926
alexanderZaporozhtsev <randallyourmaker@gmail.com>
2025-08-28 10:54:54 +0000

    add some features

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"CLI-todo/task"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello CLI-todo!")
-	task.AddTask("Test title", "Test discription", 5)
-	task.ListTasks()
+	fmt.Println("Hello CLI-todo! (type help for available commands)")
+	manager := task.NewTaskManager()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		scanner.Scan()
+		input := scanner.Text()
+		parts := strings.Fields(input)
+
+		switch parts[0] {
+		case "help":
+			manager.ListAvailableCommands()
+		case "list":
+			manager.ListTasks()
+		default:
+			fmt.Println("unknown command")
+		}
+
+	}
 }
--- task/task.go
+++ task/task.go
@@ -1,8 +1,8 @@
 package task
 
-import "fmt"
-
-var tasks []Task
+import (
+	"fmt"
+)
 
 type Task struct {
 	ID          int
@@ -12,8 +12,73 @@ type Task struct {
 	Status      bool
 }
 
-func AddTask(title, description string, priority int) Task {
-	id := len(tasks) + 1
+type Command struct {
+	Name        string
+	Description string
+}
+
+func (t Task) statusToString() string {
+	if t.Status {
+		return "[✔]"
+	}
+	return "[ ]"
+}
+
+type TaskManager struct {
+	tasks    []Task
+	commands []Command
+}
+
+func (m *TaskManager) ListAvailableCommands() {
+	fmt.Println("Available commands:")
+	fmt.Println("-------------------")
+
+	for _, cmd := range m.commands {
+		fmt.Printf("• %-10s : %s\n", cmd.Name, cmd.Description)
+	}
+
+	fmt.Println("-------------------")
+}
+
+func (m *TaskManager) initCommands() {
+	m.commands = []Command{
+		{Name: "add", Description: "Add a new task"},
+		{Name: "list", Description: "List all tasks"},
+		{Name: "status", Description: "Change task status"},
+		{Name: "delete", Description: "Delete a task"},
+		{Name: "help", Description: "Show all commands"},
+	}
+}
+
+func (m *TaskManager) ListTasksByStatus(status bool) {
+	if len(m.tasks) == 0 {
+		fmt.Println("no tasks available")
+		return
+	}
+
+	for i := range m.tasks {
+		if m.tasks[i].Status == status {
+			status := m.tasks[i].statusToString()
+
+			fmt.Printf("ID: %d\n Status: %s\n Priority: %d\n Title: %s\n Description: %s\n",
+				m.tasks[i].ID, status, m.tasks[i].Priority, m.tasks[i].Title, m.tasks[i].Description)
+		}
+	}
+}
+
+func (m *TaskManager) DeleteTask(id int) error {
+	for i := range m.tasks {
+		if id == m.tasks[i].ID {
+			m.tasks = append(m.tasks[:i], m.tasks[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("task with ID: %d not found", id)
+}
+
+func (m *TaskManager) AddTask(title, description string, priority int) {
+	id := len(m.tasks) + 1
 	task := Task{
 		ID:          id,
 		Title:       title,
@@ -22,13 +87,39 @@ func AddTask(title, description string, priority int) Task {
 		Status:      false,
 	}
 
-	tasks = append(tasks, task)
-
-	return task
+	m.tasks = append(m.tasks, task)
 }
 
-func ListTasks() {
-	for _, task := range tasks {
-		fmt.Printf("ID: %d, Title: %s, Priority: %d, Status: %t\n", task.ID, task.Title, task.Priority, task.Status)
+func (m *TaskManager) ChangeStatus(id int) (Task, error) {
+	for i := range m.tasks {
+		if id == m.tasks[i].ID {
+			m.tasks[i].Status = !m.tasks[i].Status
+			return m.tasks[i], nil
+		}
 	}
+
+	return Task{}, fmt.Errorf("task with ID: %d not found", id)
+}
+
+func (m *TaskManager) ListTasks() {
+	if len(m.tasks) == 0 {
+		fmt.Println("list is empty")
+		return
+	}
+
+	for _, t := range m.tasks {
+		status := t.statusToString()
+
+		fmt.Printf("ID: %d\n Status: %s\n Priority: %d\n Title: %s\n Description: %s\n", t.ID, status, t.Priority, t.Title, t.Description)
+	}
+}
+
+func NewTaskManager() *TaskManager {
+	m := &TaskManager{
+		tasks: []Task{},
+	}
+
+	m.initCommands()
+
+	return m
 }
